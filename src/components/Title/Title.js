fec45e0358f0f7e05133b635cc2bd1df4c6f55dc
natapas <natapas.lue@gmail.com>
2020-09-16 00:09:36 +0700

    Set margin 0

--- src/components/Title/Title.js
+++ src/components/Title/Title.js
@@ -2,7 +2,7 @@ import styles from './Title.module.scss'
 
 export const Title = ({ label }) => {
   return (
-    <h1 className={styles.Title}>
+    <h1 className={styles.title}>
       {label}
     </h1>
   )
