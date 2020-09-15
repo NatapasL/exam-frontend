20109234e7eb17a8f5f89469455b9c8906237fa4
natapas <natapas.lue@gmail.com>
2020-09-15 21:06:45 +0700

    Add home page container

---
+++ src/containers/HomePage/HomePage.js
@@ -0,0 +1,39 @@
+import { useState } from 'react'
+import { useRouter } from 'next/router'
+
+import { Layout } from '../../components/Layout'
+import { Title } from '../../components/Title'
+import { Input } from '../../components/Input'
+import { Button } from '../../components/Button'
+import styles from './HomePage.module.scss'
+
+export const HomePage = () => {
+  const [input, setInput] = useState('')
+  const router = useRouter()
+
+  const handleInputChange = (e) => {
+    setInput(e.target.value)
+  }
+
+  const handleSubmitButtonClick = () => {
+    // gql request
+    router.push('/users')
+  }
+
+  return (
+    <Layout>
+      <div className={styles.container}>
+        <Title label="ชื่อของคุณ" />
+        <div className={styles['input-container']}>
+          <Input onChange={handleInputChange}/>
+        </div>
+        {input && input.length && (
+          <Button
+            label="ยืนยัน"
+            onClick={handleSubmitButtonClick}
+          />
+        )}
+      </div>
+    </Layout>
+  )
+}
