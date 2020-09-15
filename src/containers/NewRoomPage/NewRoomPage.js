4a0144d143e4aa60eb0c6b6e74d99d5429ea8c56
natapas <natapas.lue@gmail.com>
2020-09-16 06:08:39 +0700

    Fetch data new room page

--- src/containers/NewRoomPage/NewRoomPage.js
+++ src/containers/NewRoomPage/NewRoomPage.js
@@ -1,15 +1,50 @@
+import { useEffect, useState } from 'react'
 import Link from 'next/link'
+import { useRouter } from 'next/router'
+import { useRecoilValue, useSetRecoilState } from 'recoil'
+
+import { apolloClient } from '../../infra/apollo'
 import { Layout } from '../../components/Layout'
 import { Title } from '../../components/Title'
 import { Input } from '../../components/Input'
 import { Button } from '../../components/Button'
 import { ButtonGroup } from '../../components/ButtonGroup'
 import styles from './NewRoomPage.module.scss'
+import { CREATE_ROOM } from '../../graphql/mutation/room'
+import { currentUserState } from '../../store/state/currentUser'
+import { currentRoomState } from '../../store/state/currentRoom'
 
 export const NewRoomPage = () => {
+  const [input, setInput] = useState('')
+  const router = useRouter()
+  const currentUser = useRecoilValue(currentUserState)
+  const setCurrentRoom = useSetRecoilState(currentRoomState)
+
+  useEffect(() => {
+    if (!currentUser) {
+      router.push('/')
+    }
+  })
+
+  if (!currentUser) {
+    return <div />
+  }
+
+  const handleInputChange = (e) => {
+    setInput(e.target.value)
+  }
 
   const handleSubmitButtonClick = () => {
-
+    apolloClient.mutate({
+      mutation: CREATE_ROOM,
+      variables: { name: input },
+      context: { headers: { userId: currentUser.id } }
+    }).then(({ data: { createRoom: room } }) => {
+      setCurrentRoom(room)
+      router.push(`/rooms/${room.id}`)
+    }).catch(() => {
+      alert('Create room failure')
+    })
   }
 
   return (
@@ -19,7 +54,7 @@ export const NewRoomPage = () => {
           <Title label="สร้างห้องใหม่" />
         </div>
         <div className={styles['input-container']}>
-          <Input />
+          <Input onChange={handleInputChange} />
         </div>
         <ButtonGroup>
           <Link href="/users">
