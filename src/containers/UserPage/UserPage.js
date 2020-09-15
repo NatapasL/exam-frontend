b0bdce88b2bda2c952a579b6253f3483baf76668
natapas <natapas.lue@gmail.com>
2020-09-16 06:08:49 +0700

    Fetch data user page

--- src/containers/UserPage/UserPage.js
+++ src/containers/UserPage/UserPage.js
@@ -1,18 +1,33 @@
+import { useEffect } from 'react'
 import Link from 'next/link'
+import { useRouter } from 'next/router'
+import { useRecoilValue } from 'recoil'
 
 import { Layout } from '../../components/Layout'
 import { Title } from '../../components/Title'
 import { Button } from '../../components/Button'
 import styles from './UserPage.module.scss'
+import { currentUserState } from '../../store/state/currentUser'
 
 export const UserPage = () => {
-  const userName = 'Golf'
+  const router = useRouter()
+  const currentUser = useRecoilValue(currentUserState)
+
+  useEffect(() => {
+    if (!currentUser) {
+      router.push('/')
+    }
+  })
+
+  if (!currentUser) {
+    return <div />
+  }
 
   return (
     <Layout>
       <div className={styles.container}>
         <div className={styles['title-container']}>
-          <Title label={`คุณ ${userName}`} />
+          <Title label={`คุณ ${currentUser && currentUser.name}`} />
         </div>
         <div className={styles['create-room-button-container']}>
           <Link href="/rooms/new">
