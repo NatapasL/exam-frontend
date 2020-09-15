fa1a717df829471fe2fc85ee626f5b9a3bee859b
natapas <natapas.lue@gmail.com>
2020-09-16 06:05:33 +0700

    Add recoil root

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,18 @@
+import { ApolloProvider } from '@apollo/client'
+import { RecoilRoot } from 'recoil';
+
+import { apolloClient } from '../infra/apollo'
+
 import '../styles/global.scss'
 
 function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <RecoilRoot>
+      <ApolloProvider client={apolloClient}>
+        <Component {...pageProps} />
+      </ApolloProvider>
+    </RecoilRoot>
+  )
 }
 
 export default App
