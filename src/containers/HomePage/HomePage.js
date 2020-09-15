8bfe8da775fd1eca11443be02bd2219429629b73
natapas <natapas.lue@gmail.com>
2020-09-16 06:08:23 +0700

    Fetch data homepage

--- src/containers/HomePage/HomePage.js
+++ src/containers/HomePage/HomePage.js
@@ -1,22 +1,50 @@
 import { useState } from 'react'
 import { useRouter } from 'next/router'
+import { useSetRecoilState } from 'recoil'
+import get from 'lodash/get'
 
+import { apolloClient } from '../../infra/apollo'
 import { Layout } from '../../components/Layout'
 import { Title } from '../../components/Title'
 import { Input } from '../../components/Input'
 import { Button } from '../../components/Button'
 import styles from './HomePage.module.scss'
+import { GET_USERS } from '../../graphql/query/user'
+import { CREATE_USER } from '../../graphql/mutation/user'
+import { currentUserState } from '../../store/state/currentUser'
 
 export const HomePage = () => {
   const [input, setInput] = useState('')
+  const setCurrentUser = useSetRecoilState(currentUserState)
   const router = useRouter()
 
   const handleInputChange = (e) => {
     setInput(e.target.value)
   }
 
-  const handleSubmitButtonClick = () => {
-    // gql request
+  const handleSubmitButtonClick = async () => {
+    const {
+      data: {
+        users
+      }
+    } = await apolloClient.query({
+      query: GET_USERS,
+      variables: { name: input }
+    })
+    let user = users && get(users, 0)
+    if (user) {
+      setCurrentUser(user)
+    } else {
+      const {
+        data: {
+          createUser
+        }
+      } = await apolloClient.mutate({
+        mutation: CREATE_USER,
+        variables: { name: input }
+      })
+      setCurrentUser(createUser)
+    }
     router.push('/users')
   }
 
