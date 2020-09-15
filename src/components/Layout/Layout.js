69eb2731ce0fda051095d3cb59c9c7f73f47604f
natapas <natapas.lue@gmail.com>
2020-09-16 06:13:21 +0700

    Add layout logo

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -3,6 +3,7 @@ import styles from './Layout.module.scss'
 export const Layout = ({ children }) => {
   return (
     <div className={styles.background}>
+      <img className={styles.logo} src="/logo.png" />
       <div className={styles['container-white']}>
         {children}
       </div>
